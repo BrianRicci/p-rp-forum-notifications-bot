7b7a149a1e002455a9278e79b2f6e75b198db6a7
Yunus Ibragimov <yunus.ibragimov49@gmail.com>
2025-07-20 15:44:41 +0300

    Основа для бота

---
+++ src/bot/handlers.py
@@ -0,0 +1,55 @@
+from aiogram import F, Router
+from aiogram.types import Message
+from aiogram.filters import Command, CommandObject, CommandStart
+from aiogram.utils.formatting import (Bold, Text, Code, as_marked_section, as_numbered_section)
+from aiogram.utils.markdown import link
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    content = Text(
+        Bold('Привет! Я бот для отслеживания новых сообщений на форумах.\n\n'),
+        as_marked_section(
+            Bold('📌 Доступные команды:'),
+            Text(Code("/add <ссылка>"), " — добавить топик для отслеживания."),
+            Text(Code("/list"), " — список отслеживаемых топиков."),
+            Text(Code("/remove <номер>"), " — удалить топик из списка."),
+            Text(Code("/help"), " — показать справку."),
+            marker='🔹 ',
+        ),
+        '\n\nОтправь мне ссылку на топик, и я буду присылать уведомления о новых постах!\n',
+        'По всем вопросам обращаться сюда @ricci_141',
+    )
+
+    await message.answer(**content.as_kwargs())
+
+# /add <url>
+# Здесь надо будет добавить ссылку для отслеживания с привязкой к пользователю
+@router.message(Command('add'))
+async def add_topic(message: Message, command: CommandObject):
+    url = command.args
+    await message.answer(f'url: {link('Топик', url)}')
+
+# /list
+# Здесь надо будет сделать вывод списка
+@router.message(Command('list'))
+async def list_topic(message: Message):
+    content = Text(
+        as_numbered_section(
+            Bold('📋 Ваши отслеживаемые топики:\n'),
+            link('Топик', 'https://proxima-rp.ru/threads/sly_reaver-prizyv-k-smerti-i-oskorblenie-bobbi-nrp.64493/'),
+        ),
+        '\n\nЧтобы удалить топик, используйте /remove <номер>',
+    )
+    await message.answer(**content.as_kwargs())
+
+# /remove <number>
+# Здесь надо будет сделать удаление поста из списка
+@router.message(Command('remove'))
+async def remove_topic(message: Message, command: CommandObject):
+    topic_number = command.args
+
+    await message.answer(f'Удален топик под номером: {topic_number}')
---
+++ src/bot/run.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import TG_TOKEN
+from handlers import router
+
+
+bot = Bot(
+    token=TG_TOKEN,
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.MARKDOWN_V2
+        # тут ещё много других интересных настроек
+    )
+)
+
+dp = Dispatcher()
+
+
+async def main():
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    # Включаем логирование, чтобы не пропустить важные сообщения
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
